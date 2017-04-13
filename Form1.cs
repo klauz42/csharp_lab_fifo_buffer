7a977da62f43c59cb71e252162b86e1e5ca0e8cb
klauz42 <n.matveev@outlook.com>
2017-04-13 11:07:42 +0300

    added lab

---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FIFO_Buffer
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> queueStr = new Queue<string>();
+        Queue<int> queueInt = new Queue<int>();
+        int intTmp = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                bool IsInt = int.TryParse(textBox1.Text, out intTmp);
+                if (textBox1.Text == string.Empty)
+                    MessageBox.Show("Cтрока пуста", "Ошибка");
+                else if (IsInt == false)
+                    MessageBox.Show("Введенное значение не является целочисленным числом или выходит за пределы int", "Ошибка");
+                else
+                {
+                    queueInt.Enqueue(intTmp);
+                }
+
+            }
+            catch(EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Ошибка: "+ ex.Message, "Ошибка");
+            }
+            finally
+            {
+                textBox1.Clear();
+                richTextBox1.Text = queueInt.PrintResult();
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text == string.Empty)
+                    MessageBox.Show("Cтрока пуста", "Ошибка");
+                else
+                {
+                    queueStr.Enqueue(textBox1.Text);
+                }
+
+            }
+            catch (EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка");
+            }
+            finally
+            {
+                textBox1.Clear();
+                richTextBox2.Text = queueStr.PrintResult();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (queueInt.Count() == 0)
+                    MessageBox.Show("Буфер пуст", "Ошибка");
+                else
+                {
+                    textBox3.Text = queueInt.Dequeue();
+                }
+
+            }
+            catch (EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка");
+            }
+            finally
+            {
+                richTextBox1.Text = queueInt.PrintResult();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (queueStr.Count() == 0)
+                    MessageBox.Show("Буфер пуст", "Ошибка");
+                else
+                {
+                    textBox3.Text = queueStr.Dequeue();
+                }
+
+            }
+            catch (EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка");
+            }
+            finally
+            {
+                richTextBox2.Text = queueStr.PrintResult();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (queueInt.Count() == 0)
+                    MessageBox.Show("Буфер пуст", "Ошибка");
+                else
+                {
+                    if(queueInt.Contains(int.Parse(textBox2.Text)))
+                        MessageBox.Show("Да, содержит", "Ответ");
+                    else
+                        MessageBox.Show("Нет, не содержит", "Ответ");
+                }
+
+            }
+            catch (EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка");
+            }
+            finally
+            {
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (queueStr.Count() == 0)
+                    MessageBox.Show("Буфер пуст", "Ошибка");
+                else
+                {
+                    if (textBox2.Text == string.Empty)
+                        MessageBox.Show("Введена пустая строка", "Ошибка");
+                    else
+                    {
+                        if (queueStr.Contains(textBox2.Text))
+                            MessageBox.Show("Да, содержит", "Ответ");
+                        else
+                            MessageBox.Show("Нет, не содержит", "Ответ");
+                    }
+                }
+
+            }
+            catch (EmptyList ex)
+            {
+                MessageBox.Show("Очередь пуста (" + ex.Message + ")", "Ошибка");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка");
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FIFO_Buffer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class Queue<T>
+    {
+        List<T> list = new List<T>();
+        public void Enqueue(T item)
+        {
+            list.Add(item);
+        }
+
+        public string Dequeue()
+        {
+            if (list.Count() != 0)
+            {
+                string tmp = list[0].ToString();
+                list.RemoveAt(0);
+                return tmp;
+            }
+            else throw new EmptyList();
+        }
+        public string PrintResult()
+        {
+            string Output = string.Empty;
+            foreach (var item in list)
+            {
+                Output += item.ToString() + "\n"; 
+            }
+            return Output;
+        }
+        public int Count()
+        {
+            return list.Count;
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public T GetLast()
+        {
+           return list.Last();
+        }
+
+    }
+
+    public class EmptyList: System.Exception
+    {
+
+    }
+}
